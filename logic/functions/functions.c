e2a1fb04490ca4ffaed807b1dc10d35246f4c89c
Sergi Ramos Jordan <sergi.ramos@matagalls.salleurl.edu>
2018-12-26 19:05:15 +0000

    fix: Fixed a trivial error related with the option 1 menu.
    The program doesn't print a message error when an incorrect value is entered in option 1 menu. Called the function "printOptionError" into the
    function "showAllDestinations" in file "functions.c", line 9, when the user enter an incorrect value.

--- logic/functions/functions.c
+++ logic/functions/functions.c
@@ -11,6 +11,10 @@ void showAllDestinations(List *l) {
 		printSortMenu();
 
 		option = askUserForOption();
+
+		if (option < 1 || option > MAX_SORTING){
+			printOptionError();
+		} 
 	} while (option < 1 || option > MAX_SORTING);
 
 	option -= 1;
